4b7c20ebc028cdf3cd01c91b4dd252e1b9c8a3b7
Matz Momme Klint <matzmommeklint@gmail.com>
2022-12-30 20:04:47 +0100

    use only powershell commands and no aliases

--- install.ps1
+++ install.ps1
@@ -6,26 +6,26 @@ $ProfilePath = (Get-Item $PROFILE).DirectoryName
 New-Item -type Directory "$ProfilePath\completions" -Force
 $CompletionsPath = "$ProfilePath\completions\"
 
-echo "generate Profile"
-echo "# generated Profile" > $PROFILE
+Write-Output "generate Profile"
+Write-Output "# generated Profile" > $PROFILE
 
-echo "installing completions:"
+Write-Output "installing completions:"
 $softwares = Get-Content software.yaml | ConvertFrom-Yaml 
 $softwares | Foreach-Object -ThrottleLimit 5 -Parallel {
   #Action that will run in Parallel. Reference the current object via $PSItem and bring in outside variables with $USING:varname
 
   if ($PSItem.ContainsKey("module")){
     $module = $PSItem.module
-    echo $module
+    Write-Output $module
     Install-Module $module -Scope CurrentUser -AllowClobber
-    echo "Import-Module $module" >> $USING:PROFILE
+    Write-Output "Import-Module $module" >> $USING:PROFILE
   }
   if ($PSItem.ContainsKey("command")){
     $folder = $USING:CompletionsPath
     $filePath = $PSItem.exe | % {$folder + $_ + ".ps1"}
-    echo $filePath
-    $args = $PSItem["command"].Split(" ")
-    & $PSItem["exe"] @args > $filePath
+    Write-Output $filePath
+    $cmdargs = $PSItem["command"].Split(" ")
+    & $PSItem["exe"] @cmdargs > $filePath
   }
 }
 Get-ChildItem .\completions\ *.ps1 | Copy-Item -Destination $CompletionsPath
@@ -34,5 +34,5 @@ Get-ChildItem $CompletionsPath *.ps1 | Foreach-Object  {
   ". $CompletionsPath$fileName" >> $PROFILE
 }
 Get-Content profile.ps1 >> $PROFILE
-echo "Finished Writing to Profile:"
-echo $PROFILE
+Write-Output "Finished Writing to Profile:"
+Write-Output $PROFILE
