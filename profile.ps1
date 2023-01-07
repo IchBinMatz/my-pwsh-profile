66933631757f4aad13982fe2c0c663615177b1c5
Matz Momme Klint <matzmommeklint@gmail.com>
2023-01-07 18:36:48 +0100

    changed stuff

--- profile.ps1
+++ profile.ps1
@@ -2,4 +2,6 @@
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
 $repos = "$env:HOMEPATH\Documents\repos"
-oh-my-posh init pwsh | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/paradox.omp.json" | Invoke-Expression
+# disable VenvPrompt because the oh-my-posh-theme will show it anyway
+$env:VIRTUAL_ENV_DISABLE_PROMPT = 1
